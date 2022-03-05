373c118aa14f117755944af1e5e4fac25bd0e675
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-05 04:14:03 +0100

    added tip section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tip-app">
+      <div className="tip-app-container">
+        <div className="tip-app-content">
+          <div className="bill-section">
+            <label>Bill</label>
+            <div>
+              <p className="dollar">$</p>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="tip-section">
+            <p>Select Tip %</p>
+            <div className="tip-section-container">
+              <button>5%</button>
+              <button>10%</button>
+              <button>15%</button>
+              <button>20%</button>
+              <button>25%</button>
+              <button>Custom</button>
+            </div>
+          </div>
+          <div className="bill-section">
+            <label>Number of People</label>
+            <div>
+              <p className="dollar">$</p>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="summary-section-container">
+            <div className="summary-section">
+              <div className="tip-amount-summary-section">
+                <div className="tip-amount-section">
+                  <p className="tip-amount">Tip Amount</p>
+                  <p className="per">/ person</p>
+                </div>
+                <p>$</p>
+              </div>
+              <div className="tip-amount-summary-section">
+                <div className="tip-amount-section">
+                  <p className="tip-amount">Total</p>
+                  <p className="per">/ person</p>
+                </div>
+                <p>$</p>
+              </div>
+            </div>
+            <button>Reset</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
