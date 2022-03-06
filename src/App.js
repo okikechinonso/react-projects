355c969923c08169b727bf450c1d31b44004af1d
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-06 09:32:09 +0100

    restarted the css version

--- src/App.js
+++ src/App.js
@@ -3,34 +3,49 @@ import "./App.css";
 
 function App() {
   return (
+    // <div></div>
     <div className="tip-app">
+      <div className="tip-title">
+        <div>SPLI</div> <div>TTER</div>
+      </div>
       <div className="tip-app-container">
         <div className="tip-app-content">
-          <div className="bill-section">
-            <label>Bill</label>
-            <div>
-              <p className="dollar">$</p>
-              <input type="text" />
+          <div className="desktop">
+            <div className="bill-section">
+              <label>Bill</label>
+              <div>
+                <span className="dollar">$</span>
+                <input type="number" />
+              </div>
+            </div>
+            <div className="tip-section">
+              <lable>Select Tip %</lable>
+              <div className="tip-section-container">
+                <button>5%</button>
+                <button>10%</button>
+                <button>15%</button>
+                <button>20%</button>
+                <button>25%</button>
+                <button className="custom">Custom</button>
+              </div>
+            </div>
+            <div className="bill-section">
+              <label>Number of People</label>
+              <div>
+                <svg
+                  height={24}
+                  width="24"
+                  className="dollar"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
+                </svg>
+                <input type="text" />
+              </div>
             </div>
           </div>
-          <div className="tip-section">
-            <p>Select Tip %</p>
-            <div className="tip-section-container">
-              <button>5%</button>
-              <button>10%</button>
-              <button>15%</button>
-              <button>20%</button>
-              <button>25%</button>
-              <button>Custom</button>
-            </div>
-          </div>
-          <div className="bill-section">
-            <label>Number of People</label>
-            <div>
-              <p className="dollar">$</p>
-              <input type="text" />
-            </div>
-          </div>
+
           <div className="summary-section-container">
             <div className="summary-section">
               <div className="tip-amount-summary-section">
@@ -38,14 +53,14 @@ function App() {
                   <p className="tip-amount">Tip Amount</p>
                   <p className="per">/ person</p>
                 </div>
-                <p>$</p>
+                <p className="tip-summary-dollar">$4.27</p>
               </div>
               <div className="tip-amount-summary-section">
                 <div className="tip-amount-section">
                   <p className="tip-amount">Total</p>
                   <p className="per">/ person</p>
                 </div>
-                <p>$</p>
+                <p className="tip-summary-dollar">$32.79</p>
               </div>
             </div>
             <button>Reset</button>
