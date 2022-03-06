7a8ef241d707ec020d431f622676a7064b28700d
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-06 21:16:01 +0100

    complete the logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [tipAmount, setTipAmount] = useState(0);
+  const [tipRate, setTipRate] = useState(5);
+  const [people, setPeople] = useState(null);
+  const [peramount, setPerAmount] = useState(0);
+
+  const onClictButton = (e) => {
+    setTipAmount(
+      ((amount * (e.target.value / 100)) / people).toPrecision(4)
+    );
+    setTipRate(e.target.value / 100);
+    setPerAmount((c) => (c = e.target.value / people)).toPrecision(4);
+      
+    console.log(tipRate);
+  };
+  const onChangeAmount = (e) => {
+      if (e.target.value.trim() === "") {
+        setAmount(0);
+        setTipAmount(0);
+      }
+
+      if( people == null && e.target.value.trim() !== ""){
+        setAmount(e.target.value);
+        setTipAmount(
+          ((e.target.value * (tipRate / 100)) / 1).toPrecision(4)
+        );
+        setPerAmount((c) => (c = e.target.value / 1)).toPrecision(4);
+      }
+      
+      
+    
+  };
+  const onChangePeople = (e) => {
+    if (e.target.value.trim() === "") {
+      setPeople((c) => (c = 1));
+      setTipAmount(
+        (c) => (c = ((amount * (tipRate / 100)) / 1).toPrecision(4))
+      );
+      setPerAmount(amount/1)
+    } else {
+      setPeople((c) => (c = e.target.value));
+      setTipAmount(
+        (c) =>
+          (c = ((amount * (tipRate / 100)) / e.target.value).toPrecision(5))
+      );
+      setPerAmount((c) => (c = amount / e.target.value));
+      console.log(amount + "tip");
+    }
+  };
+
+  const onReset = () =>{
+    setAmount("")
+    setPeople("")
+    setPerAmount(0)
+    setTipRate(5)
+    setTipAmount(0)
+  }
+
   return (
-    // <div></div>
     <div className="tip-app">
       <div className="tip-title">
         <div>SPLI</div> <div>TTER</div>
@@ -15,22 +73,42 @@ function App() {
               <label>Bill</label>
               <div>
                 <span className="dollar">$</span>
-                <input type="number" />
+                <input
+                  onChange={onChangeAmount}
+                  type="number"
+                  name="amount"
+                  placeholder="Amount"
+                  value={amount}
+                />
               </div>
             </div>
             <div className="tip-section">
-              <lable>Select Tip %</lable>
+              <label>Select Tip %</label>
+              <br />
               <div className="tip-section-container">
-                <button>5%</button>
-                <button>10%</button>
-                <button>15%</button>
-                <button>20%</button>
-                <button>25%</button>
-                <button className="custom">Custom</button>
+                <button onClick={onClictButton} className="tip" value="5">
+                  5%
+                </button>
+                <button onClick={onClictButton} className="tip" value="10">
+                  10%
+                </button>
+                <button onClick={onClictButton} className="tip" value="15">
+                  15%
+                </button>
+                <button onClick={onClictButton} className="tip" value="20">
+                  20%
+                </button>
+                <button onClick={onClictButton} className="tip" value="25">
+                  25%
+                </button>
+                <button onClick={onClictButton} className="custom" value="5">
+                  Custom
+                </button>
               </div>
             </div>
             <div className="bill-section">
               <label>Number of People</label>
+              <br />
               <div>
                 <svg
                   height={24}
@@ -39,9 +117,20 @@ function App() {
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
                 >
-                  <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
+                  <g color="hsl(184, 14%, 56%)">
+                    <path
+                      fill="currentcolor"
+                      d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
+                    />
+                  </g>
                 </svg>
-                <input type="text" />
+                <input
+                  onChange={onChangePeople}
+                  type="number"
+                  name="people"
+                  placeholder="No. of People"
+                  value={people}
+                />
               </div>
             </div>
           </div>
@@ -53,17 +142,17 @@ function App() {
                   <p className="tip-amount">Tip Amount</p>
                   <p className="per">/ person</p>
                 </div>
-                <p className="tip-summary-dollar">$4.27</p>
+                <p className="tip-summary-dollar">${tipAmount}</p>
               </div>
               <div className="tip-amount-summary-section">
                 <div className="tip-amount-section">
                   <p className="tip-amount">Total</p>
                   <p className="per">/ person</p>
                 </div>
-                <p className="tip-summary-dollar">$32.79</p>
+                <p className="tip-summary-dollar">${peramount}</p>
               </div>
             </div>
-            <button>Reset</button>
+            <button onClick={onReset}>Reset</button>
           </div>
         </div>
       </div>
